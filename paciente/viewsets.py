ebd9a9bd1362bbdbc9c52cd71f14f438d336b9d5
João Victor Medeiros <114639835+ViictorDantas@users.noreply.github.com>
2024-09-22 19:40:12 -0300

    Revert "Marge back para front"

--- paciente/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from paciente.models import Paciente
-from paciente.serializers import PacienteSerializer
-
-class PacienteViewSet(viewsets.ModelViewSet):
-    queryset = Paciente.objects.all()
-    serializer_class = PacienteSerializer
--- usuario/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from usuario.models import Usuario
-from usuario.serializers import UsuarioSerializer
-
-class UsuarioViewSet(viewsets.ModelViewSet):
-    queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
