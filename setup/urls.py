f95bce5e88e986c65c5a85274e22c762fef13535
ViictorDantas <viictordantaz@gmail.com>
2024-10-21 18:30:13 -0300

    implementando login

--- setup/urls.py
+++ setup/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 from usuario.router import usuario_router
 from paciente.router import paciente_router
 from endereco.router import endereco_router
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
@@ -12,4 +16,6 @@ urlpatterns = [
     path('api/v1/', include(usuario_router.urls)),
     path('api/v1/', include(paciente_router.urls)),
     path('api/v1/', include(endereco_router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
