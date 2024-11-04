cdfb494c1acce1381dbeef142a9b8703652f1dc3
ViictorDantas <viictordantaz@gmail.com>
2024-11-04 17:49:28 -0300

    adicionando swagger

--- setup/urls.py
+++ setup/urls.py
@@ -5,11 +5,25 @@ from django.conf.urls.static import static
 from usuario.router import usuario_router
 from paciente.router import paciente_router
 from endereco.router import endereco_router
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Projeto ESaúde",
+      default_version='v1',
+      description="Sistema desenvolvido com Django e Django REST Framework, oferece uma plataforma completa para gerenciamento de pacientes, médicos, secretários e consultas.",
+      contact=openapi.Contact(email="viictordantaz@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,4 +32,7 @@ urlpatterns = [
     path('api/v1/', include(endereco_router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
